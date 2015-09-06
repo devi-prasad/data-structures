7fe2e17b8bc5204d3c8d84e67f4b0706121b97cc
dprasad <dprasadm@gmail.com>
2015-09-06 12:09:09 +0530

    Fixed white spaces.

--- queue/queue-basic/queue.c
+++ queue/queue-basic/queue.c
@@ -4,16 +4,16 @@
 
 Queue  queue_new(uint32_t size)
 {
-	size = (size > 0 && size < MAX_QUEUE_LEN) ? size : MAX_QUEUE_LEN;
-	
+    size = (size > 0 && size < MAX_QUEUE_LEN) ? size : MAX_QUEUE_LEN;
+
     Queue q = {size, /*count*/ 0, /*head*/0, /*tail*/0, /*data*/{0} };
-    
+
     return q;
 }
 
 Queue* queue_add(Queue *q, int32_t data, QueueResult *result)
 {
-	assert(q != NULL && q->count <= q->size);
+    assert(q != NULL && q->count <= q->size);
 
     if (q->count < q->size) {
         q->q[q->tail] = data;
@@ -30,7 +30,7 @@ Queue* queue_add(Queue *q, int32_t data, QueueResult *result)
 
 Queue* queue_remove(Queue *q, QueueResult *result)
 {
-	assert(q != NULL && q->count <= q->size);
+    assert(q != NULL && q->count <= q->size);
 
     if (q->count > 0) {
         result->data = q->q[q->head];
@@ -49,11 +49,11 @@ Queue* queue_remove(Queue *q, QueueResult *result)
 
 uint32_t queue_full(Queue *q)
 {
-	assert(q != NULL && q->count <= q->size);
+    assert(q != NULL && q->count <= q->size);
     return (q->count == q->size);
 }
 
 uint32_t queue_empty(Queue *q)
 {
-    return (q->count == 0);
+    return (q->count == 0 && q->head == q->tail);
 }
